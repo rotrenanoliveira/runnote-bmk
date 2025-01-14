32228569112058e3646dbf316661dd4c4e18a25e
Renan Oliveira <rotrenanoliveira@gmail.com>
2025-01-14 10:13:17 -0300

    feat: setup puppeteer

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,9 +1,10 @@
-import { Header } from '@/components/header'
-
 export default function Home() {
   return (
-    <div>
-      <Header />
+    <div className="flex flex-col min-h-screen w-screen p-6">
+      {/* <Header /> */}
+      <main className="w-full flex flex-col items-center py-6 space-y-4">
+        <h1>Runnote:bmk</h1>
+      </main>
     </div>
   )
 }
