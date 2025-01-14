9eb5ff4e11d7c7240ae0e52b7bc1316a4e52256f
Renan Oliveira <rotrenanoliveira@gmail.com>
2025-01-14 10:46:05 -0300

    feat: add hook useFormState

---
+++ src/hooks/use-form-state.ts
@@ -0,0 +1,57 @@
+import { type FormEvent, type RefObject, useState, useTransition } from 'react'
+
+type FormState = {
+  success: boolean
+  message: string
+}
+
+export function useFormState(
+  action: (data: FormData) => Promise<FormState>,
+  opts?: {
+    /** Callback when the form has an error. */
+    onError?: (message: string) => void
+    /** Callback when the form has a success. */
+    onSuccess?: (message?: string) => Promise<void> | void
+    /** Initial data for the form. */
+    initialData?: FormState
+    /** This will reset the form state when the action is successful. */
+    reset?: boolean
+    /** This is needed to access the form element. */
+    formRef?: RefObject<HTMLFormElement | null>
+  },
+) {
+  const initialData = opts?.initialData
+
+  const [isTransitioning, startTransition] = useTransition()
+
+  const [result, setResult] = useState(initialData ?? { success: false, message: '' })
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const form = event.currentTarget
+    const data = new FormData(form)
+
+    startTransition(async () => {
+      const result = await action(data)
+
+      if (result.success === false && opts?.onError) {
+        return opts.onError(result.message)
+      }
+
+      if (result.success === true && opts?.onSuccess) {
+        if (opts?.reset && opts.formRef) {
+          form.reset()
+        }
+
+        return await opts.onSuccess(result.message)
+      }
+
+      setResult(result)
+    })
+  }
+
+  const isPending = isTransitioning
+
+  return [result, handleSubmit, isPending] as const
+}
